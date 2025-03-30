aaa0f1e896b55f1430eb19b229c2e142e9f109c1
Diego Amicabile <diego.amicabile@soprasteria.com>
2025-03-30 18:17:55 +0200

    Terraform try

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -3,30 +3,34 @@
 
 resource "random_pet" "prefix" {}
 
+
+
 provider "azurerm" {
+  skip_provider_registration = true
   features {}
 }
 
 resource "azurerm_resource_group" "default" {
   name     = "${random_pet.prefix.id}-rg"
-  location = "West US 2"
+  location = "West Europe" # Updated to West Europe
 
   tags = {
     environment = "Demo"
   }
 }
-
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "${random_pet.prefix.id}-aks"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
-  kubernetes_version  = "1.26.3"
+  kubernetes_version  = "1.29"  # Updated to a supported version
+  
+  # Rest of your configuration...
 
   default_node_pool {
     name            = "default"
     node_count      = 2
-    vm_size         = "Standard_D2_v2"
+    vm_size         = "Standard_d2s_v5" # Changed to an allowed VM size
     os_disk_size_gb = 30
   }
 
